c6317694fa0f07b180175551712e22c9957aa969
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2025-01-14 12:55:47 +0200

    Update onGetTasks.ts

--- src/utils/api/onGetTasks.ts
+++ src/utils/api/onGetTasks.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { handleAsyncOperationErrors } from './handleAsyncOperationError';
 import type { ITask } from '@/types/Task.types';
 
-axios.defaults.baseURL = 'https://localhost:7249/api';
+axios.defaults.baseURL = 'https://todo-list-backend-d1xw.onrender.com/api';
 
 export const onGetTasks = async () => {
   return await handleAsyncOperationErrors<ITask[]>(async (): Promise<ITask[]> => {
